787edc63cf1a77abf33ca93bebb19a47c129b0b0
UltWolf <core.safecrlf>
2018-07-15 15:22:12 +0300

    Добавьте файлы проекта.

---
+++ DapperExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DapperExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
